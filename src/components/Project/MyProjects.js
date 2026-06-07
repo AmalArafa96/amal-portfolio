9fc935148e197c73509a96879a004302115e6174
AmalArafa96 <eng.amal.arafa@gmail.com>
2026-06-07 15:44:24 +0200

    first commit

---
+++ src/components/Project/MyProjects.js
@@ -0,0 +1,74 @@
+import img0 from "../../assets/img/bakery.jpg";
+import img1 from "../../assets/img/ecomProj.png";
+import img2 from "../../assets/img/oop-game.webp";
+import img3 from "../../assets/img/yummyProj.png";
+import img4 from "../../assets/img/wheaterProj.png";
+import img5 from "../../assets/img/prayer.jpg";
+export const myProjects = [
+  {
+    id: 0,
+    projectTitle: "Family Bakery",
+    projDesc: "Bakery Landing Page",
+    projTools: "I used Tools : ",
+    tools: ["Html", "BootStrab", "Css"],
+    gitHubLink: "https://github.com/AmalArafa96/bakery.git",
+    liveLink: "https://amalarafa96.github.io/bakery/",
+    category: ["css"],
+    imgPath: img0,
+  },
+  {
+    id: 1,
+    projectTitle: "E-commerce",
+    projDesc: "Online Store",
+    projTools: "I used Tools : ",
+    tools: ["React", "Tailwind", "API", "Redux", "vite"],
+    gitHubLink: "https://github.com/AmalArafa96/e-commerce.git",
+    liveLink: "https://amalarafa96.github.io/e-commerce/",
+    category: ["react"],
+    imgPath: img1,
+  },
+  {
+    id: 2,
+    projectTitle: " Oop Games",
+    projDesc: "A modular OOP-based game library ",
+    projTools: "I used Tools : ",
+    tools: ["Bootstrap", "JavaScript", "OOP", "api"],
+    gitHubLink: "https://github.com/AmalArafa96/Movie-Game-oop.git",
+    liveLink: " https://amalarafa96.github.io/Movie-Game-oop/",
+    category: ["js"],
+    imgPath: img2,
+  },
+  {
+    id: 3,
+    projectTitle: "Yummy Food",
+    projDesc: "Menue Desisn",
+    projTools: "I used Tools : ",
+    tools: ["BootStrab", "JavaScript", "Api", "JQuery"],
+    gitHubLink: "https://github.com/AmalArafa96/Ymmy-App.git",
+    liveLink: "https://amalarafa96.github.io/Ymmy-App/",
+    category: ["js"],
+    imgPath: img3,
+  },
+  {
+    id: 4,
+    projectTitle: "Wheather App",
+    projDesc: "Menue Desisn",
+    projTools: "I used Tools : ",
+    tools: ["BootStrab", "JavaScript", "Api"],
+    gitHubLink: "https://github.com/AmalArafa96/Weather-App.git",
+    liveLink: "https://amalarafa96.github.io/Weather-App/",
+    category: ["js"],
+    imgPath: img4,
+  },
+  {
+    id: 5,
+    projectTitle: "Prayer Times",
+    projDesc: "Islamic Prayer times",
+    projTools: "I used Tools : ",
+    tools: ["React", "Tailwind", "API", "vite"],
+    gitHubLink: "https://github.com/AmalArafa96/prayertimes.git",
+    liveLink: "https://amalarafa96.github.io/prayertimes/",
+    category: ["react"],
+    imgPath: img5,
+  },
+];
